4afaf1559419da82b9825abb5aea108e966212ba
MikhayilGM <99989816+MikhayilGM@users.noreply.github.com>
2023-01-23 14:38:03 +0400

    Update caesar.h

--- caesar.h
+++ caesar.h
@@ -8,14 +8,15 @@ class Caesar
     private:
         void dencryption();
         void encryption();
+        char sym_enc(char);
+        char sym_denc(char);
     public:
         void set_encfname(std::string);
         void set_dencfname(std::string);
         void set_enc_num(int);
     
     private:
-        char sym_enc(char);
-        char sym_denc(char);
+
         std::string input {};
         std::string output {};
         int encrip {};
